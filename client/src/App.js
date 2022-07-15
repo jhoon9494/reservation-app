d1f32ffcbc1bacf53a699f6787b87ffb9aeaefc5
Wafu <dacapolin@gmail.com>
2022-07-15 21:22:05 +0900

    Update: pull후 수정 후 진행

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-<<<<<<< HEAD
 import MyPage from './pages/MyPage';
-=======
 import Site from './pages/Site';
->>>>>>> f98f6988b96507b70c0c3abdc6373c66a436d2bb
 
 function App() {
   return (
@@ -13,11 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
           <Route path="/MyPage" element={<MyPage />} />
-=======
           <Route path="/site" element={<Site />} />
->>>>>>> f98f6988b96507b70c0c3abdc6373c66a436d2bb
         </Routes>
       </BrowserRouter>
     </>
