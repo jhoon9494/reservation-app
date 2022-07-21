ee047ca4157891c7af0c7a5c04a419035b6cb1ca
junghoon <wjdgns9406@gmail.com>
2022-07-21 14:17:01 +0900

    Feat: 객실 상세 모달창 이미지 슬라이더 구현 및 관련 코드 수정

--- src/components/RoomModal.jsx
+++ src/components/RoomModal.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import '../styles/modal.css';
-import { VscChevronLeft, VscChevronRight } from 'react-icons/vsc';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import baseStyle from '../styles/baseStyle';
 import RoomTabs from './RoomTabs';
+import RoomImageSlider from './RoomImageSlider';
 import RoomDetailContent from './RoomDetailContent';
 import RoomReviews from './RoomReviews';
 import axios from 'axios';
@@ -14,7 +14,7 @@ import axios from 'axios';
 export const RoomModal = ({ show, onHide, roomID }) => {
   const [currTab, setCurrTab] = useState('객실 설명');
   const [roomContent, setRoomContent] = useState({});
-  const [imgZoom, setImgZoom] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,11 +22,7 @@ export const RoomModal = ({ show, onHide, roomID }) => {
       // RoomsButton 컴포넌트에서 초기 roomID값은 empty string임.
       // roomID에 값이 들어올때만 api 요청
       if (roomID !== '') {
-        const res = await axios.get('http://localhost:5000/api/roomInfo', {
-          params: {
-            roomId: roomID,
-          },
-        });
+        const res = await axios.get(`http://localhost:5000/api/room/${roomID}`);
         setRoomContent(res.data);
       }
     }
@@ -40,32 +36,19 @@ export const RoomModal = ({ show, onHide, roomID }) => {
         onHide();
         // 모달창을 종료하고 다시 켰을 때 모달창 내부 초기화
         setCurrTab('객실 설명');
-        setImgZoom(false);
       }}
       dialogClassName="my-modal"
       aria-labelledby="roomTitle"
       centered
     >
-      <Header id="roomTitle">{roomContent?.name}</Header>
-      {/* TODO 이미지 캐러셀로 슬라이드 구현 */}
-      <ImgContainer onClick={() => setImgZoom((prev) => !prev)}>
-        {/* roomContent의 초기값은 빈 배열이므로 인덱스를 통해 접근할 수 없음.
-        객실을 선택하여 roomID에 값이 들어가야만 인덱스를 통해 접근 할 수 있음.
-        값이 없을 경우 undefined를 반환하여 에러를 발생하지 않도록 옵셔널 체이닝(?.)을 사용 */}
-        <VscChevronLeft />
-        <Image
-          src={roomContent?.imgSrc}
-          alt={roomContent?.name}
-          zoom={imgZoom}
-        />
-        <VscChevronRight />
-      </ImgContainer>
+      <Header id="roomTitle">{roomContent.name}</Header>
+      <RoomImageSlider roomContent={roomContent} />
       <ContentContainer>
         <RoomTabs currTab={currTab} setCurrTab={setCurrTab} />
         {currTab === '객실 설명' ? (
           <RoomDetailContent roomData={roomContent} />
         ) : (
-          <RoomReviews roomID={roomContent?.objectID} />
+          <RoomReviews roomID={roomContent._id} />
         )}
       </ContentContainer>
 
@@ -81,25 +64,6 @@ const Header = styled.div`
   font-size: ${baseStyle.titleFontSize};
   margin: 54px auto auto;
   text-align: center;
-`;
-
-const ImgContainer = styled.div`
-  display: flex;
-  width: 480px;
-  height: 286px;
-  margin: 21px auto auto;
-  background-color: #0000003b;
-`;
-
-const Image = styled.img`
-  width: 90%;
-  height: 100%;
-  transition: all ease 250ms 0ms;
-  ${(props) =>
-    props.zoom &&
-    css`
-      transform: scale(2) translate(0, 50px);
-    `}
 `;
 
 const ContentContainer = styled.div`
