0a0b7e70fce9f17b99ba0bbf7f4e0ed21bf8e4ec
junghoon <wjdgns9406@gmail.com>
2022-07-22 21:47:11 +0900

    Fix: 선택한 일정 사이에 예약 불가능한 날짜가 있는 경우 선택할 수 없도록 수정

--- src/components/DateRangePick.jsx
+++ src/components/DateRangePick.jsx
@@ -7,6 +7,8 @@ const { RangePicker } = DatePicker;
 
 const DateRangePick = ({ setDate, roomID }) => {
   const [reservedDates, setReservedDates] = useState([]);
+  const [dateRange, changeDateRange] = useState(null);
+
   useEffect(() => {
     // 예약 불가능한 날짜 데이터를 받기 전 배열 초기화
     setReservedDates([]);
@@ -37,10 +39,28 @@ const DateRangePick = ({ setDate, roomID }) => {
       );
     });
   };
-  const handleDate = (date, dateString) => {
+  const handleDate = (date) => {
+    const startDate = moment(date[0], 'YYYY-MM-DD');
+    const endDate = moment(date[1], 'YYYY-MM-DD');
+
+    changeDateRange([
+      moment(date[0], 'YYYY-MM-DD'),
+      moment(date[1], 'YYYY-MM-DD'),
+    ]);
+
+    // 선택한 일정 사이에 예약 불가능한 날짜가 있는 경우 경고창으로 알려줌
+    if (
+      reservedDates.some((date) => {
+        return moment(date).isBetween(startDate, endDate, 'day', []);
+      })
+    ) {
+      changeDateRange([]);
+      return alert('일정을 다시 확인해주세요.');
+    }
+
     setDate({
-      startDate: new Date(dateString[0]),
-      endDate: new Date(dateString[1]),
+      startDate: startDate._d,
+      endDate: endDate._d,
     });
   };
   return (
@@ -49,6 +69,7 @@ const DateRangePick = ({ setDate, roomID }) => {
         disabledDate={disabledDate}
         size={'large'}
         onChange={handleDate}
+        value={dateRange}
       />
     </>
   );
